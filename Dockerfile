cf58244319e8101a51bb66387c4c59e8ed566eb5
junni01kim <audwnssp@naver.com>
2024-10-04 00:10:09 +0900

    명칭 BaseProject로 변경

--- Dockerfile
+++ Dockerfile
@@ -6,4 +6,4 @@ ARG JAR_FILE_PATH=build/libs/daou-0.0.1-SNAPSHOT.jar
 
 COPY ${JAR_FILE_PATH} daou-0.0.1-SNAPSHOT.jar
 
-ENTRYPOINT ["java", "-jar", "daou-0.0.1-SNAPSHOT.jar", "-Dspring.profiles.active=local"]
+ENTRYPOINT ["java", "-jar", "project-0.0.1-SNAPSHOT.jar", "-Dspring.profiles.active=local"]
--- src/main/java/com/daou/BaseBackEndApplication.java
+++ src/main/java/com/daou/BaseBackEndApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DaouBackEndApplication {
+public class BaseBackEndApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DaouBackEndApplication.class, args);
+		SpringApplication.run(BaseBackEndApplication.class, args);
 	}
 
 }
