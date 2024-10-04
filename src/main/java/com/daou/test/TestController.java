cc4a8f4376768d2c439c01f23ada601ff0c265bb
junni01kim <audwnssp@naver.com>
2024-10-04 22:57:40 +0900

    준희야 고마워

--- src/main/java/com/daou/test/TestController.java
+++ src/main/java/com/daou/test/TestController.java
@@ -8,24 +8,24 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/v1/test/")
 @RequiredArgsConstructor // 자동 DI
 public class TestController {
-//    private final TestService testService;
+    private final TestService testService;
 
     @GetMapping("test")
     public ResponseEntity test() {
         return ResponseEntity.builder().status(200).message("테스트 성공").data("테스트 성공").build();
     }
-//
-//    @PostMapping("signup")
-//    public ResponseEntity testSignup(@RequestBody Signup signup) {
-//        var user = testService.signup(signup);
-//
-//        return ResponseEntity.builder().status(200).message("계정 생성 성공").data(user).build();
-//    }
-//
-//    @PostMapping("login")
-//    public ResponseEntity testLogin(@RequestBody Login login) {
-//        var user = testService.login(login);
-//
-//        return ResponseEntity.builder().status(200).message("로그인 성공").data(user).build();
-//    }
+
+    @PostMapping("signup")
+    public ResponseEntity testSignup(@RequestBody Signup signup) {
+        var user = testService.signup(signup);
+
+        return ResponseEntity.builder().status(200).message("계정 생성 성공").data(user).build();
+    }
+
+    @PostMapping("login")
+    public ResponseEntity testLogin(@RequestBody Login login) {
+        var user = testService.login(login);
+
+        return ResponseEntity.builder().status(200).message("로그인 성공").data(user).build();
+    }
 }
